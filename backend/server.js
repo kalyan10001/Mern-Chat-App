8e6cfc6769ff4db0907faf3af6e2a16d8925a6d8
kalyan10001 <venkatakalyan1000@gmail.com>
2024-05-23 19:33:31 +0530

    deploy changes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
+dotenv.config();
 import cookieParser from "cookie-parser";
 
 import connectToMongoDB from "./db/connectToMongoDb.js";
@@ -14,7 +14,6 @@ import {app, server} from './socket/socket.js'
 const PORT=process.env.PORT || 5000;
 
 const __dirname=path.resolve();
-dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -30,7 +29,7 @@ app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dir,"frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
 })
 
 server.listen(PORT,()=>
