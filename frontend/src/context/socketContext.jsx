5ccaff8cd73aa9f2bea834cc594f75d51f770851
kalyan10001 <venkatakalyan1000@gmail.com>
2024-05-23 19:57:11 +0530

    adding socket live location

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider=({children})=>{
     useEffect(()=>{
         if(authUser)
             {
-                const socket=io("http://localhost:5000",{
+                const socket=io("https://mern-chat-app-1-k9bw.onrender.com/",{
                     query:{
                         userId:authUser._id
                     }
